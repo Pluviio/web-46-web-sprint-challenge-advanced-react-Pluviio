ed4d07d895003fba7d95bcbe7948c989c52e90ef
courtneyharris <harriscourtney98@gmail.com>
2021-09-03 15:19:05 -0400

    Moving on to tests, everything seems to be working

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,35 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialvalue) => {
+    const [value, setValue] = useState(() => {
+        if (localStorage.getItem(key)) {
+            return (JSON.parse(localStorage.getItem(key)));
+        } else {
+            localStorage.setItem(key, JSON.stringify(initialvalue));
+            return (initialvalue);
+        }
+    })
+
+    const setStoredValue = (value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+        setValue(value);
+    }
+
+    return ([value, setStoredValue]);
+}
+
+const useForm = (initialValues) => {
+    const [values, setValues] = useLocalStorage('form', initialValues);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    return([values, handleChanges]);
+}
+
+export default useForm;
